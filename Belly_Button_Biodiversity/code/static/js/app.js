f6cc3fde3c28630a9ad0d3b49b1b695407e3385d
harsh-env <mpddriller@live.com>
2022-06-27 03:14:20 -0500

    made bar chart with Sub_ID for otu_ids and Sample_values

--- Belly_Button_Biodiversity/code/static/js/app.js
+++ Belly_Button_Biodiversity/code/static/js/app.js
@@ -3,59 +3,66 @@ const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 
 // Identifying the data structure using console.log
 d3.json(url).then(function(data){
-    // console.log(data)
+    // console.log(data);
 
     let metadata = data.metadata;
     let ID_names = data.names;
     let samples = data.samples;
-    // console.log("ID_Names:",ID_names)
-
-    // Build a list in dropdownmenu:
+    // console.log("ID_Names:",ID_names);
+function init() {
+    console.log("Initialize the graphs on webpage");
+    // Build a list in dropdownmenu, appending it in list "option" & Value equal to Sub ID
     let dropdownMenu = d3.select("#selDataset");
     for (let i = 0; i < ID_names.length; i++){
-    //   dropdownMenu.append("option").text(ID_names[i]).attr("value", ID_names[i]);
-    dropdownMenu.append("option").text(ID_names[i]).attr("value", ID_names[i]);
+      dropdownMenu.append("option").text(ID_names[i]).attr("value", ID_names[i]);
+    // dropdownMenu.append("option").text(ID_names[i]);
     };
-   
-});
-d3.selectAll("#selDataset").on("change", optionChanged)
-function optionChanged(){
+    createBar(ID_names[0]);
+
+
+d3.selectAll("#selDataset").on("change", optionChanged);
+    
+    function optionChanged(){
     // code that updates graphics
-    let id = d3.select("#selDataset").property("value");
-        console.log(id);
-    // createBar(id);
+        let dp_menu = d3.select("#selDataset");
+        let dp_ID = dp_menu.property("value");
+        console.log(dp_ID);
+        createBar(dp_ID);
+    };
 };
 
-function createBar(id){
-    for (let i = 0; i < names.length; i++){
+    function createBar(dp_ID){
+        for (let i = 0; i < ID_names.length; i++){
       // finds the index where the current id is
-      if (id == ID_names[i]){
+            if (dp_ID == ID_names[i]){
         // Stores top 10 OTU data at current id
-        let sample_values = samples[i].sample_values.slice(0, 10);
-        let temp = samples[i].otu_ids.slice(0, 10);
-        let otu_ids = temp.map(function (x) {
-          return "OTU " + x;
-        });
-        let otu_labels = samples[i].otu_labels.slice(0, 10);
-        // Sets up the data for horizontal bar chart at current id
-        let data = [{
-        type: 'bar',
-        x: sample_values,
-        y: otu_ids,
-        text: otu_labels,
-        transforms: [{
-          type: 'sort',
-          target: 'x',
-          order: 'ascending'
-        }],
-        orientation: 'h'
-        }];
+            let sample_values = samples[i].sample_values.slice(0, 10);
+            let temp = samples[i].otu_ids.slice(0, 10);
+            let otu_ids = temp.map(function (x) {
+            return "OTU " + x;
+            });
+            let otu_labels = samples[i].otu_labels.slice(0, 10);
+            // Sets up the data for horizontal bar chart at current id
+            let data1 = [{
+                type: 'bar',
+                x: sample_values,
+                y: otu_ids,
+                text: otu_labels,
+                transforms: [{
+                type: 'sort',
+                target: 'x',
+                order: 'ascending'
+                }],
+                orientation: 'h'
+                }];
         //Creates the bar chart with plotly at id='bar'
-        Plotly.newPlot("bar", data);
+            Plotly.newPlot("bar", data1);
         //Exits the loop
-        break;
-      };
-    };
+            break;
+            };
+        };
     // checking to see if function is running
-    console.log(`This function generates bar chart of ${id} `);
-  };
+        console.log(`This function generates bar chart of ${dp_ID} `);
+    };
+    init();
+});
